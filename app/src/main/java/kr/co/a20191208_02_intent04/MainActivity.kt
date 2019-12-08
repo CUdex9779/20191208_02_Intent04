8401947a5312be394b67facd6ce36001397624d1
CUdex9779 <cuyu9779@gmail.com>
2019-12-08 12:10:25 +0900

    전화연결 CALL 인텐트 연습 - 권한 관련문제로 아직 실행불가.,

--- app/src/main/java/kr/co/a20191208_02_intent04/MainActivity.kt
+++ app/src/main/java/kr/co/a20191208_02_intent04/MainActivity.kt
@@ -1,12 +1,30 @@
 package kr.co.a20191208_02_intent04
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dialBtn.setOnClickListener {
+//            누구에게 전화를 걸리 정보를 미리 명시
+            val uri = Uri.parse("tel:010-1111-2222")
+//            어느 화면을 넘겨줄지 명시
+            val Intent = Intent(Intent.ACTION_DIAL,uri)
+            startActivity(Intent)
+        }
+
+        callBtn.setOnClickListener {
+            val uri = Uri.parse("tel:010-1111-2222")
+            val Inte = Intent(Intent.ACTION_CALL,uri)
+            startActivity(Inte)
+        }
+
     }
 }
